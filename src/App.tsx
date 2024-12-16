34db8a585c69315391514e4c94f5b8bbfd15ea84
GeonhyoPark815 <rjsgy0815@gmail.com>
2024-12-16 22:57:32 +0900

    [Fix] Prevent pinch zoom on APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Canvas from "./pages/Canvas/Canvas";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    function listener(event: TouchEvent) {
+      // Prevent pinch zoom
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    }
+
+    window.addEventListener("touchmove", listener, { passive: false });
+
+    return () => {
+      window.removeEventListener("touchmove", listener);
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
