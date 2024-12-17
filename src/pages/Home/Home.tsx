7bfe05362f37daeb53bcca8c93cc639aa31f1b7b
GeonhyoPark815 <rjsgy0815@gmail.com>
2024-12-18 00:51:09 +0900

    [Style] Change HomeScreen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,23 @@
-import React from "react";
 import styles from "./Home.module.css";
-import MainSection from "./MainSection";
-import FloatingButton from "./Button";
 
 const Home: React.FC = () => {
   return (
     <div className={styles.home}>
-      <MainSection />
-      <FloatingButton />
+      <img className={styles.image} src="images/main.png" />
+      <div className={styles.footer}>
+        <a href="/2024-12-21" className={styles.button}>
+          <span className={styles.label}>롤링페이퍼 작성하기</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.icon}
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M13.172 12l-4.95-4.95 1.414-1.414L17 12l-7.364 7.364-1.414-1.414z" />
+          </svg>
+        </a>
+        <p className={styles.date}>2024.12.21</p>
+      </div>
     </div>
   );
 };
